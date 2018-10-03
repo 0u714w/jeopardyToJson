241ab5a534f20022afcb4ab56fa6b3191a93cf95
Doug Enas <douglassenas@gmail.com>
2018-10-03 19:41:55 -0400

    looking better

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-const fs = require("fs")
+let fs = require("fs")
+var fetch = require("node-fetch");
 
 
-let temp = fs.writeFile("http://jservice.io/api/category?id=42")
+let categoryOne = fetch("http://jservice.io/api/category?id=42")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categoryOne)
 
-console.log(temp)
+let categoryTwo = fetch("http://jservice.io/api/category?id=37")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categoryTwo)
 
+let categoryThree = fetch("http://jservice.io/api/category?id=139")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categoryThree)
 
-fetch("http://jservice.io/api/category?id=42")
-fs.writeFile("categories.json")
+let categoryFour = fetch("http://jservice.io/api/category?id=1079")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categoryFour)
 
-fetch("http://jservice.io/api/category?id=37")
-fs.writeFile("categories.json")
+let categoryFive = fetch("http://jservice.io/api/category?id=777")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categoryFive)
 
-fetch("http://jservice.io/api/category?id=139")
-fs.writeFile("categories.json")
-
-fetch("http://jservice.io/api/category?id=1079")
-fs.writeFile("categories.json")
-
-fetch("http://jservice.io/api/category?id=777")
-fs.writeFile("categories.json")
-
-fetch("http://jservice.io/api/category?id=539")
-fs.writeFile("categories.json")
+let categorySix = fetch("http://jservice.io/api/category?id=539")
+    .then(res => res.json())
+    .then(data => {
+        let newData = data
+        console.log(newData)
+    })
+fs.writeFileSync('categories.json', categorySix)
