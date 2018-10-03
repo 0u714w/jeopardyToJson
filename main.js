7a92afa9fe5ae801685d1ee2a8a2dc118efd9f6e
Doug Enas <douglassenas@gmail.com>
2018-10-03 15:00:56 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+fetch("http://jservice.io/api/category?id=42")
+fs.writeFile("categories.json")
+
+fetch("http://jservice.io/api/category?id=37")
+fs.writeFile("categories.json")
+
+fetch("http://jservice.io/api/category?id=139")
+fs.writeFile("categories.json")
+
+fetch("http://jservice.io/api/category?id=1079")
+fs.writeFile("categories.json")
+
+fetch("http://jservice.io/api/category?id=777")
+fs.writeFile("categories.json")
+
+fetch("http://jservice.io/api/category?id=539")
+fs.writeFile("categories.json")
